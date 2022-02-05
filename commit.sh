f70f3354e89282e1e0195ba67cbfebf00e04bde5
bioinfosif <ndaom403@gmail.com>
2022-02-05 17:37:31 +0000

    commit_msg

--- commit.sh
+++ commit.sh
@@ -8,4 +8,4 @@ read commit_msgs
 git add .
 git commit -m commit_msg
 git pull --rebase
-git push --set-upstream https://user_name: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@github.com/bioinfosif/job_ML.git main
+git push --set-upstream https://user_name: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@github.com/bioinfosif/job_ML.git main
